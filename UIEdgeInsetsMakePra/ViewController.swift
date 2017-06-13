db2f12b760ae36a943c1a0f0102262d7f36ef3d7
Lingzt <asklingtoby@gmail.com>
2017-06-13 10:55:49 -0400

    learn adjust button hit/tab section by adjusting the content EdgeInsets

--- UIEdgeInsetsMakePra/ViewController.swift
+++ UIEdgeInsetsMakePra/ViewController.swift
@@ -16,7 +16,9 @@ class ViewController: UIViewController {
 //        self.button.imageEdgeInsets = UIEdgeInsetsMake (200,0,0,0) //move down 200  下移200
 //        self.button.imageEdgeInsets = UIEdgeInsetsMake (-200,0,0,0) //move 200 down 上移200
 //        self.button.imageEdgeInsets = UIEdgeInsetsMake (0,0,200,0) //move 200 down
-        self.button.imageEdgeInsets = UIEdgeInsetsMake (200,0,200,0) //stretch 200 up and down both ways 上下拉升各200，图片被拉长
+        self.button.imageEdgeInsets = UIEdgeInsetsMake (200,200,200,200) //stretch 200 up and down both ways 上下拉升各200，图片被拉长
+        
+        self.button.contentEdgeInsets = UIEdgeInsetsMake (0,200,200,200) //contentEdge of the button UIView. It only shows in UIHierarchy view// 调整大小玩 看看背景content view 是怎么变化的。
 //        self.button.titleEdgeInsets = UIEdgeInsetsMake (200,0,0,0)
     }
 
